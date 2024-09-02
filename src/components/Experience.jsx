293fd3e8922bf78b42642724ed859f55da35f3cf
BISTREL <bistrel.tsangue@algosup.com>
2024-09-02 13:17:35 +0100

    Creating my 3D portfolio
    Introduction, creating my 3D character

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,43 @@
-import { OrbitControls } from "@react-three/drei";
+import {ContactShadows, Environment, OrbitControls, Sky} from "@react-three/drei";
+import {Bistrel} from "./Bistrel.jsx";
+import {useControls} from "leva";
 
 export const Experience = () => {
+
+    const {animations} = useControls({
+        animations: {
+            value: "Typing",
+            options: ["Typing", "Standing", "Falling 1"],
+        }
+    })
+
   return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
+      <>
+          <OrbitControls/>
+          <Sky/>
+          <Environment preset="apartment"/>
+          <group position-y={-1}>
+              <Bistrel animation={animations}/>
+              {animations === "Typing" && (
+                  <mesh scale={[0.8, 0.5, 0.7]} position-y={0.18}>
+                      <boxGeometry/>
+                      <meshStandardMaterial color="blue"/>
+                  </mesh>
+              )}
+
+              <ContactShadows
+                  opacity={0.6}
+                  scale={10}
+                  blur={1}
+                  far={10}
+                  resolution={256}
+                  color="#FFFFFF" />
+
+              <mesh scale={10} rotation-x={-Math.PI * 0.5} position-y={-0.007}>
+                  <planeGeometry/>
+                  <meshStandardMaterial color="black"/>
+              </mesh>
+          </group>
+      </>
   );
 };
